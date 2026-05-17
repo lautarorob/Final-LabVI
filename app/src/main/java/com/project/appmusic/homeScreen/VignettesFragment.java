2041a58e5e36aba10e209faf96e34538547b4e32
Lautaro Robles <robleslautaro54@gmail.com>
2026-05-17 15:56:30 -0300

    modelado de vignettes

--- app/src/main/java/com/project/appmusic/homeScreen/VignettesFragment.java
+++ app/src/main/java/com/project/appmusic/homeScreen/VignettesFragment.java
@@ -1,66 +1,63 @@
 package com.project.appmusic.homeScreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.project.appmusic.R;
+import com.project.appmusic.likedSongsScreen.LikedScreen;
+import com.project.appmusic.regionalScreen.RegionalScreen;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VignettesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class VignettesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TextView txtTitulo;
+    private ImageView imgVignette;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public VignettesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VignettesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VignettesFragment newInstance(String param1, String param2) {
-        VignettesFragment fragment = new VignettesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_vignettes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        txtTitulo = view.findViewById(R.id.txt_titulo);
+        imgVignette = view.findViewById(R.id.img_portada);
+
+        String typeVignette = getTag();
+
+
+        if (typeVignette != null && typeVignette.equals("Ilike")) {
+
+            if (txtTitulo != null) txtTitulo.setText(R.string.tus_me_gusta);
+            if (imgVignette != null) imgVignette.setImageResource(R.drawable.like_logo);
+            view.setOnClickListener(v -> {
+                Intent intent = new Intent(getContext(), LikedScreen.class);
+                startActivity(intent);
+            });
+
+        } else {
+
+            if (txtTitulo != null) txtTitulo.setText(R.string.regionales);
+            if (imgVignette != null) imgVignette.setImageResource(R.drawable.regional_logo);
+            view.setOnClickListener(v -> {
+                Intent intent = new Intent(getContext(), RegionalScreen.class);
+                startActivity(intent);
+            });
+
+        }
     }
 }
